0a603de0f59ec556c7cdc0c7d9b5f9c5103858c8
SamLaThien <92208283+SamLaThien@users.noreply.github.com>
2024-09-12 23:52:39 +0700

    Update levelItemChances.js

--- utils/levelItemChances.js
+++ utils/levelItemChances.js
@@ -1,154 +1,22 @@
 
 export const levelItemChances = {
-  '0-10': {
-    83: 5,  // Huyết Khí Đan
-  },
-  '11-20': {
-    83: 3,  // Huyết Khí Đan
-    44: 15, // Đế Giai Thuẫn
-    45: 20, // Tị Lôi Châu
-    46: 30, // Thanh Tâm Đan
-    47: 10, // Hộ Linh Trận
-  },
-  '21-30': {
-    83: 2,  // Huyết Khí Đan
-    44: 8,  // Đế Giai Thuẫn
-    45: 20, // Tị Lôi Châu
-    46: 30, // Thanh Tâm Đan
-    47: 10, // Hộ Linh Trận
-  },
-  '31-40': {
-    83: 1,  // Huyết Khí Đan
-    44: 5,  // Đế Giai Thuẫn
-    45: 13, // Tị Lôi Châu
-    46: 23, // Thanh Tâm Đan
-    47: 10, // Hộ Linh Trận
-  },
-  '41-50': {
-    83: 0,  // Huyết Khí Đan
-    44: 2,  // Đế Giai Thuẫn
-    45: 10, // Tị Lôi Châu
-    46: 15, // Thanh Tâm Đan
-    47: 5,  // Hộ Linh Trận
-  },
-  '51-60': {
-    83: 0,  // Huyết Khí Đan
-    44: 2,  // Đế Giai Thuẫn
-    45: 10, // Tị Lôi Châu
-    46: 15, // Thanh Tâm Đan
-    47: 5,  // Hộ Linh Trận
-  },
-  '61-70': {
-    83: 0,  // Huyết Khí Đan
-    44: 2,  // Đế Giai Thuẫn
-    45: 10, // Tị Lôi Châu
-    46: 15, // Thanh Tâm Đan
-    47: 5,  // Hộ Linh Trận
-  },
-  '71-80': {
-    83: 0,  // Huyết Khí Đan
-    44: 2,  // Đế Giai Thuẫn
-    45: 10, // Tị Lôi Châu
-    46: 15, // Thanh Tâm Đan
-    47: 5,  // Hộ Linh Trận
-  },
-  '81-90': {
-    83: 0,  // Huyết Khí Đan
-    44: 2,  // Đế Giai Thuẫn
-    45: 10, // Tị Lôi Châu
-    46: 15, // Thanh Tâm Đan
-    47: 5,  // Hộ Linh Trận
-  },
-  '91-100': {
-    83: 0,  // Huyết Khí Đan
-    44: 2,  // Đế Giai Thuẫn
-    45: 10, // Tị Lôi Châu
-    46: 15, // Thanh Tâm Đan
-    47: 5,  // Hộ Linh Trận
-  },
+  0: [48, 49, 50, 51, 80],
+  1: [83],
+  2: [44],
+  3: [45],
+  4: [46],
+  5: [47],
 };
 
 export const consistentItemChances = {
+  83: 5,  // Huyết Khí Đan
+  44: 15, // Đế Giai Thuẫn
+  45: 20, // Tị Lôi Châu
+  46: 30, // Thanh Tâm Đan
+  47: 10, // Hộ Linh Trận
   51: 4,   // Hoả Ngọc Châu
   80: 5,   // Hoàng Kim Lệnh
   50: 5,   // Thái Ngọc Châu
   49: 10,  // Sa Ngọc Châu
   48: 30,  // Tân Lôi Trận
 };
-
-// export const levelItemChances = {
-//   '0-10': {
-//     35: 5,  // Huyết Khí Đan
-//   },
-//   '11-20': {
-//     35: 3,  // Huyết Khí Đan
-//     38: 15, // Đế Giai Thuẫn
-//     39: 20, // Tị Lôi Châu
-//     40: 30, // Thanh Tâm Đan
-//     41: 10, // Hộ Linh Trận
-//   },
-//   '21-30': {
-//     35: 2,  // Huyết Khí Đan
-//     38: 8,  // Đế Giai Thuẫn
-//     39: 20, // Tị Lôi Châu
-//     40: 30, // Thanh Tâm Đan
-//     41: 10, // Hộ Linh Trận
-//   },
-//   '31-40': {
-//     35: 1,  // Huyết Khí Đan
-//     38: 5,  // Đế Giai Thuẫn
-//     39: 13, // Tị Lôi Châu
-//     40: 23, // Thanh Tâm Đan
-//     41: 10, // Hộ Linh Trận
-//   },
-//   '41-50': {
-//     35: 0,  // Huyết Khí Đan
-//     38: 2,  // Đế Giai Thuẫn
-//     39: 10, // Tị Lôi Châu
-//     40: 15, // Thanh Tâm Đan
-//     41: 5,  // Hộ Linh Trận
-//   },
-//   '51-60': {
-//     35: 0,  // Huyết Khí Đan
-//     38: 2,  // Đế Giai Thuẫn
-//     39: 10, // Tị Lôi Châu
-//     40: 15, // Thanh Tâm Đan
-//     41: 5,  // Hộ Linh Trận
-//   },
-//   '61-70': {
-//     35: 0,  // Huyết Khí Đan
-//     38: 2,  // Đế Giai Thuẫn
-//     39: 10, // Tị Lôi Châu
-//     40: 15, // Thanh Tâm Đan
-//     41: 5,  // Hộ Linh Trận
-//   },
-//   '71-80': {
-//     35: 0,  // Huyết Khí Đan
-//     38: 2,  // Đế Giai Thuẫn
-//     39: 10, // Tị Lôi Châu
-//     40: 15, // Thanh Tâm Đan
-//     41: 5,  // Hộ Linh Trận
-//   },
-//   '81-90': {
-//     35: 0,  // Huyết Khí Đan
-//     38: 2,  // Đế Giai Thuẫn
-//     39: 10, // Tị Lôi Châu
-//     40: 15, // Thanh Tâm Đan
-//     41: 5,  // Hộ Linh Trận
-//   },
-//   '91-100': {
-//     35: 0,  // Huyết Khí Đan
-//     38: 2,  // Đế Giai Thuẫn
-//     39: 10, // Tị Lôi Châu
-//     40: 15, // Thanh Tâm Đan
-//     41: 5,  // Hộ Linh Trận
-//   },
-// };
-
-// export const consistentItemChances = {
-//   45: 4,   // Hoả Ngọc Châu
-//   44: 5,   // Hoàng Kim Lệnh
-//   44: 5,   // Thái Ngọc Châu
-//   43: 10,  // Sa Ngọc Châu
-//   42: 30,  // Tân Lôi Trận
-// };
