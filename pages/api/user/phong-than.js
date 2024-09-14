989966707bd09b7d0f4d10ac6a13fbce380eb27e
SamLaThien <92208283+SamLaThien@users.noreply.github.com>
2024-09-14 22:20:29 +0700

    Update phong-than.js

--- pages/api/user/phong-than.js
+++ pages/api/user/phong-than.js
@@ -15,7 +15,7 @@ export default async function handler(req, res) {
 
         // Query to get top 10 users based on level
         const getTopUsersByLevel = `
-        SELECT id, username, ngoai_hieu
+        SELECT id, username, ngoai_hieu, clan_role
         FROM users
         WHERE id > 5
         ORDER BY level DESC
@@ -23,13 +23,13 @@ export default async function handler(req, res) {
 
         // Query to get top 10 users based on tai_san
         const getTopUsersByTaiSan = `
-        SELECT id, username, ngoai_hieu
+        SELECT id, username, ngoai_hieu, clan_role
         FROM users
         WHERE id > 5
         ORDER BY tai_san DESC
         LIMIT 10;`;
 
-        const getTopBangHoi = `SELECT name FROM clans;`;
+        const getTopBangHoi = `SELECT name,id FROM clans;`;
 
         // Execute both queries and process results
         db.query(getTopUsersByLevel, (error, topUsersResults) => {
@@ -41,7 +41,10 @@ export default async function handler(req, res) {
             // Modify results to use ngoai_hieu if available, otherwise fallback to username
             dsTop.topUsers = topUsersResults.map(user => ({
                 id: user.id,
-                username: user.ngoai_hieu || user.username
+                user: {
+                    username: user.ngoai_hieu || user.username,
+                    class: getName(user.clan_role || 'danden')
+                }
             }));
 
             // After getting topUsers, execute the next query for topTaiSans
@@ -54,7 +57,10 @@ export default async function handler(req, res) {
                 // Modify results to use ngoai_hieu if available, otherwise fallback to username
                 dsTop.topTaiSans = topTaiSansResults.map(user => ({
                     id: user.id,
-                    username: user.ngoai_hieu || user.username
+                    user: {
+                        username: user.ngoai_hieu || user.username,
+                        class: getName(user.clan_role || 'danden')
+                    }
                 }));
                 db.query(getTopBangHoi, (error, topBangHoiResults) => {
                     if (error) {
@@ -64,7 +70,8 @@ export default async function handler(req, res) {
 
                     // Modify results to use ngoai_hieu if available, otherwise fallback to username
                     dsTop.topBangHois = topBangHoiResults.map(user => ({
-                        name: user.name
+                        name: user.name,
+                        class: getName(user.id)
                     }));
                     // Respond with the dsTop array containing both sets of top users
                     return res.status(200).json(dsTop);
@@ -74,4 +81,18 @@ export default async function handler(req, res) {
     } catch (error) {
         return res.status(500).json({ message: 'Internal server error', error: error.message });
     }
+}
+
+function getName(name) {
+    const id = {
+        1: 'thienmon',
+        2: 'vinhhang',
+        9: 'voquan'
+    };
+
+    if (id.hasOwnProperty(name)) {
+        return id[name];
+    }
+
+    return name;
 }
