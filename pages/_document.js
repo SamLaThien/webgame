5e72087e203ab84303c5eeace7b5979c9bf19fc4
SamLaThien <92208283+SamLaThien@users.noreply.github.com>
2024-09-14 22:20:47 +0700

    Update _document.js

--- pages/_document.js
+++ pages/_document.js
@@ -1,4 +1,5 @@
-import Document from 'next/document';
+// pages/_document.js
+import Document, { Html, Head, Main, NextScript } from 'next/document';
 import { ServerStyleSheet } from 'styled-components';
 
 class MyDocument extends Document {
@@ -26,6 +27,25 @@ class MyDocument extends Document {
     } finally {
       sheet.seal();
     }
+  }
+
+  render() {
+    return (
+      <Html>
+        <Head>
+          {/* Thêm liên kết đến Google Fonts */}
+          <link
+            rel="stylesheet"
+            href="https://fonts.googleapis.com/css2?family=Bilbo+Swash+Caps&family=Mrs+Sheppards&family=Splash&display=swap"
+          />
+          {/* Bạn có thể thêm các liên kết khác hoặc meta tags ở đây */}
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
   }
 }
 
