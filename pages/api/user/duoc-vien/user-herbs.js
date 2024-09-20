0c8d0e0949c9d33d595661eee4841c716d9e4128
SamLaThien <92208283+SamLaThien@users.noreply.github.com>
2024-09-20 20:43:02 +0700

    Update user-herbs.js

--- pages/api/user/duoc-vien/user-herbs.js
+++ pages/api/user/duoc-vien/user-herbs.js
@@ -18,19 +18,13 @@ export default async function handler(req, res) {
     const decoded = jwt.verify(token, process.env.JWT_SECRET);
     const userId = decoded.userId;
 
-    db.query(`
-      SELECT uh.id, uh.herb_id, uh.name, uh.pham_cap, uh.grow_time, uh.createdAt, uh.endAt, uh.isGrown, uh.isCollected,
-             u.username, u.ngoai_hieu
-      FROM user_herbs uh
-      JOIN users u ON uh.user_id = u.id
-      WHERE uh.user_id = ?
-    `, [userId], (err, results) => {
+    db.query(`SELECT * FROM user_herbs`, [userId], (err, results) => {
       if (err) {
         return res.status(500).json({ message: "Internal server error", error: err.message });
       }
 
       if (results.length === 0) {
-        return res.status(200).json([]); 
+        return res.status(200).json([]);
       }
 
       return res.status(200).json(results);
